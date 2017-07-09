381c3d3e04c31dbbc4ce0feee628dd8bd74b4d48
Ansèlm Joseph <an23lm@Anselms-MacBook-Pro.local>
2017-07-10 00:15:36 +0530

    Initial Commit

---
+++ Player/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Player
+//
+//  Created by Ansèlm Joseph on 10/07/2017.
+//  Copyright © 2017 Ansèlm Joseph. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Player/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Player
+//
+//  Created by Ansèlm Joseph on 10/07/2017.
+//  Copyright © 2017 Ansèlm Joseph. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
